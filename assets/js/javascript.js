0fd9e10236cf312d18e38000b3cfaff90de68c42
codeAchsh <code.achsh@gmail.com>
2025-01-02 16:23:57 +0900

    Iniitial commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,19 @@
+// document.querySelector('.hamburger-menu').addEventListener('click', function() {
+//     var menu = document.getElementById('menu');
+//     menu.classList.toggle('show');
+// });
+
+
+// function toggleMenu() {
+//     var menu = document.getElementById("menu");
+//     if (menu.style.display === "block") {
+//         menu.style.display = "none";
+//     } else {
+//         menu.style.display = "block";
+//     }
+// }
+
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+}
